dd8f0171aeecc6b8a269b87f50371e989c51a8d7
mishagavura <1mgavura@gmail.com>
2022-07-26 02:30:32 -0400

    asf

--- back/settings.py
+++ back/settings.py
@@ -121,7 +121,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'front/build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -205,9 +204,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
